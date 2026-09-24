2d644172ce0e70c3db416fa3e019a6c54e948618
Warlos123 <carlitos.manuel01@gmail.com>
2026-09-24 16:38:57 -0600

    final_quiz

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
-Dining · C
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
- 
 
-#define NUM_PHILS 5 //number of Philosophers
+#define NUM_PHILS 11 //number of Philosophers
 #define TIMES_TO_EAT 5 //number of times a Philosopher has to eat 
 
 void *philosopher(void *num);
@@ -14,24 +12,46 @@ void think(int phil);
 void eat(int phil);
 
 
-sem_t room;
-sem_t chopstick[NUM_PHILS];
+sem_t room;  
+sem_t chopstick[NUM_PHILS];  // Shared resources
 
 
 
 int main(void){
-    
 
+    sem_init(&room,0,NUM_PHILS -1);   // NUM_PHILS -1 max number of phil at the table at the same time
+
+    for(int i = 0; i < NUM_PHILS; i++){
+        sem_init(&chopstick[i], 0, 1);
+    }
+
+    int ids[NUM_PHILS];
+    pthread_t threads[NUM_PHILS]; 
+
+    for(int i = 0; i < NUM_PHILS; i++ ){  //Create NUM_PHILS of threads
+        ids[i] = i; 
+        pthread_create(&threads[i], NULL, philosopher, &ids[i]);
+    }
+        
+    for(int i = 0; i < NUM_PHILS; i++){
+        pthread_join(threads[i], NULL);
+    }
+    
+    sem_destroy(&room);
+    for(int i = 0; i < NUM_PHILS; i++){
+        sem_destroy(&chopstick[i]);
+    }
+    return 0; 
 }
 
 void think(int phil) {
     printf("Philosopher %d is thinking\n", phil);
-    sleep(1);
+    // sleep(1); //Sleep could be usefull for showing the parallelism in real time. 
 }
 
 void eat(int phil) {
     printf("Philosopher %d is eating\n", phil);
-    sleep(1);
+    // sleep(1);
 }
 
 void *philosopher(void *num){
@@ -40,9 +60,8 @@ void *philosopher(void *num){
 
     int left = phil;
     int right = (phil + 1) % NUM_PHILS;
-    int rounds;
 
-    for (rounds = 0; rounds < TIMES_TO_EAT; rounds++) { //each philosopher eats exactly TIMES_TO_EAT times, then leaves the room.
+    for (int rounds = 0; rounds < TIMES_TO_EAT; rounds++) { //each philosopher eats exactly TIMES_TO_EAT times, then leaves the room.
 
         think(phil);
  
